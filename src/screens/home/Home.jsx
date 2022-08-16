59c1d79c1e1864311b034ff3851ef46e40700b63
Mohamed Sabry <30502846+Mohamedfadah@users.noreply.github.com>
2022-08-16 11:02:48 +0200

    create Upload Button Component

---
+++ src/screens/home/Home.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import Upload from '../../components/upload/Upload';
+import { Wrapper } from './Home.styles';
+
+const Home = () => {
+  return (
+    <Wrapper>
+      <Upload />
+
+    </Wrapper>
+  )
+}
+
+export default Home;
